06750c3e875cf28798e4888787370eb5fe49a3a3
mariamamgad18 <mariamamgad182002@gmail.com>
2025-11-15 12:06:15 +0200

    - Uses ApiManager to call POST login API with email & password.   - Validates inputs, handles server responses and network errors.   - Prints success or error messages in console.

---
+++ lib/Api/ApiConstants.dart
@@ -0,0 +1,3 @@
+class ApiConstants {
+  static String baseUrl = "https://route-movie-apis.vercel.app/";
+}
---
+++ lib/Api/ApiEndpoint.dart
@@ -0,0 +1,4 @@
+class Apiendpoint {
+  static String Url = "https://route-movie-apis.vercel.app/auth/login";
+
+}
