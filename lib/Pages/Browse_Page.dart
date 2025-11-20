a28da009b77b1da943e074679476ac3062372ec7
kholod ali <kholod27112003@gmail.com>
2025-11-20 03:12:52 -0800

    feat: add BrowsePage placeholder - Created BrowsePage widget as a placeholder - Currently throws UnimplementedError - Will implement browse functionality in future

---
+++ lib/Pages/Browse_Page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class BrowsePage extends StatelessWidget{
+  const BrowsePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
